505f956e1325116323300b3d2f394d1696519106
liuyuanyuan <liuyuanyuan@pccpa.cn>
2022-07-22 11:28:44 +0800

    1

---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述
+ *
+ * @author: scott
+ * @date: 2022年07月21日 13:44
+ */
+
+@SpringBootApplication
+public class App {
+public static void main(String[] args){
+    SpringApplication.run(App.class,args);
+    System.out.println("fdsafgd");
+}
+
+}
